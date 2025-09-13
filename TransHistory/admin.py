59b7abbeae15d030cabc61185c4d0c3866d8b76c
zjp <1078223306@qq.com>
2025-09-13 10:31:49 +0800

    register HistoryNode and Contributor models for admin

--- TransHistory/admin.py
+++ TransHistory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import HistoryNode, Contributor
 
-# Register your models here.
+@admin.register(HistoryNode)
+class HistoryNodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time', 'field', 'theme', 'region', 'tag', 'created_at')
+    search_fields = ('title', 'con', 'field', 'theme', 'region', 'tag')
+    list_filter = ('field', 'theme', 'region', 'tag', 'created_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'title', 'node', 'created_at')
+    search_fields = ('name', 'email', 'title')
+    list_filter = ('created_at', 'node')
+    ordering = ('created_at',)
+    readonly_fields = ('name', 'email', 'title', 'node', 'created_at')
