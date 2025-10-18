3895d6935fbb3785e603028fa2ceb68947edfc17
zjp <1078223306@qq.com>
2025-10-19 00:36:47 +0800

    fix cover image path error

--- TransHistory/views/martor.py
+++ TransHistory/views/martor.py
@@ -51,7 +51,7 @@ def images_uploader(request):
             img_uuid = "{0}-{1}".format(uuid.uuid4().hex[:10], image.name.replace(' ', '-'))
             tmp_file = os.path.join(settings.MARTOR_UPLOAD_PATH, img_uuid)
             def_path = default_storage.save(tmp_file, ContentFile(image.read()))
-            img_url = os.path.join(settings.MEDIA_URL, def_path)
+            img_url = os.path.join(def_path)
 
             data = json.dumps({
                 'status': 200,
